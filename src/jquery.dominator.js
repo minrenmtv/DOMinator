6542c87ca59d96e0e60c5021c49a70c1e39da59e
Min Ren <minrenmtv@gmail.com>
2012-07-25 11:37:59 -0400

    clean up to pass jslint

--- src/jquery.dominator.js
+++ src/jquery.dominator.js
@@ -8,11 +8,11 @@
         var defaults = {
             concurrentBuilds: 2,
             onSomeEvent: function() {}
-        }
+        };
 
         var plugin = this;
 
-        plugin.settings = {}
+        plugin.settings = {};
 
         var init = function() {
             plugin.settings = $.extend({}, defaults, options);
@@ -27,21 +27,20 @@
             //commence the building
             for (var i = 0; i < plugin.settings.concurrentBuilds; i++) {   //loop the number of times
                 buildMe(plugin.settings.buildOrderArray.shift());           //initiate building chain
-           };
+            }
             
-        }
+        };
 
         plugin.foo_public_method = function() {
             // code goes here
-        }
+        };
         
         //buildMe function - private
         var buildMe = function(el) {
             if (plugin.settings.currentBuilds < plugin.settings.concurrentBuilds) {     //if there are fewer current builds than total builds
                 plugin.settings.currentBuilds ++;
-                el.slideDown(function(){triggerNextEl()});
-            }  
-        }
+                el.slideDown(function(){triggerNextEl();});}
+        };
 
         //callBack after element is done building - private
         var triggerNextEl = function(){
@@ -52,12 +51,9 @@
             if (plugin.settings.buildOrderArray.length > 0) {           //If there are any left in the array...
                 buildMe(plugin.settings.buildOrderArray.shift());       //Build the next one
             }
-        }
-
+        };
         init();
 
-    }
+    };
 
 })(jQuery);
-
-//some testing comments
